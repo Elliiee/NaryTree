9b8d8e7ab4ab6cf97821c3ebc35513c520ed0fe2
Elliiee <danyiluozhang@gmail.com>
2020-02-20 16:52:05 -0600

    encode and decode nary tree to binary tree

---
+++ Recursion/src/EncodeNaryToBinaryDFS.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class EncodeNaryToBinaryDFS {
+    // Encodes an n-ary tree to a binary tree.
+    public TreeNode encode(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode newRoot = new TreeNode(root.val);
+
+        // Encode the first child of n-ary node to the left node of binary tree.
+        if (root.children.size() > 0) {
+            Node firstChild = root.children.get(0);
+            newRoot.left = this.encode(firstChild);
+        }
+
+        // Encoding the rest of the sibling nodes.
+        TreeNode sibling = newRoot.left;
+        for (int i = 1; i < root.children.size(); ++i) {
+            sibling.right = this.encode(root.children.get(i));
+            sibling = sibling.right;
+        }
+
+        return newRoot;
+    }
+
+    // Decodes your binary tree to an n-ary tree.
+    public Node decode(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node newRoot = new Node(root.val, new ArrayList<Node>());
+
+        // Decoding all the children nodes
+        TreeNode sibling = root.left;
+        while (sibling != null) {
+            newRoot.children.add(this.decode(sibling));
+            sibling = sibling.right;
+        }
+
+        return newRoot;
+    }
+}
---
+++ Recursion/src/TreeNode.java
@@ -0,0 +1,9 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int _val){
+        this.val = _val;
+    }
+}
